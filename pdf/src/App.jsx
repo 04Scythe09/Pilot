fc470b3629f486f60d2cffa96df490208ec38874
Yusef Dong <Yusefdong@gmail.com>
2025-04-09 12:40:20 -0400

    Main
    Main

---
+++ pdf/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="white-box">
+      </div>
+      <div className="input-container">
+        <input 
+          type="text" 
+          className="question-input" 
+          placeholder="Ask questions"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
